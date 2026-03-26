8c0624fdabd53f7b64bf97c4505d0f3eb6da0dc8
Kyle <kbenton2023@gmail.com>
2026-03-25 21:54:38 -0400

    Update lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Kyle Benton" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hi, I'm Kyle, a sophomore from Massachusetts.  A fun fact about me is that I love CAD modeling/design and am working to get my CSWA.")
 
 if __name__ == "__main__":
     main()
